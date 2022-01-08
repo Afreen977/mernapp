3424ef0d54e2e182cda088ed3ae3b3291f65ee33
Afreen977 <93537473+Afreen977@users.noreply.github.com>
2022-01-08 08:09:36 +0530

    Delete index.js

--- client/functions/index.js
+++
@@ -1,9 +0,0 @@
-export const imageSource=(user)=>{
-    console.log(`user in imageSource is ${user}`)
-   if(user.image){
-       return user.image.url
-   }else{
-       console.log("entered in else of imageSource")
-       return "/images/cloudy.jpg"
-   }
-}
